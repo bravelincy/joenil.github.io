40db44763d61f4a58a69e4564b6a713469d33072
joenil <joenillam@gmail.com>
2015-10-16 01:45:53 +0800

    左侧菜单

--- js/base.js
+++ js/base.js
@@ -4,8 +4,20 @@ define(function (require, exports, module) {
 		return elements.length > 1 ? elements : elements[0]; 
 	}
 
+	$.isMobile = /mobile/ig.test(navigator.userAgent);
+
 	$.on = function (element, evtName, fn, useCapture) {
+		if (evtName === 'tap') {
+			evtName = this.isMobile ? 'touchstart' : 'click';	
+		}
 		element.addEventListener(evtName, fn, useCapture);
+	};
+
+	$.off = function (element, evtName, fn, useCapture) {
+		if (evtName === 'tap') {
+			evtName = this.isMobile ? 'touchstart' : 'click';	
+		}
+		element.removeEventListener(evtName, fn, useCapture);
 	};
 
 	return $;
--- js/resume.js
+++ js/resume.js
@@ -1,25 +1,85 @@
 require(['base'], function ($) {
 	var initPageTime = Date.now();
-	var body = $('body');
-	var mainPanel = $('.panel-main');
-	var scrollBtn = $('#scroll-btn');
-	var banner = $('.banner');
+
+	//TODO:带锚点进来的布局会乱
+	if (location.hash) {
+		location.replace(location.href.replace(/#\w+/,''));
+	}
 
 	function checkReadyState() {
 		if (document.readyState === 'complete') {
 			var now = Date.now();
 			var costTime = now - initPageTime;
 
-			setTimeout(function () {
-				body.classList.remove('loading');
-			}, 999 - costTime);
+			setTimeout(initPage, 999 - costTime);
+		}
+	}
+
+	function initPage() {
+		var body = $('body');
+		var mainPanel = $('.panel-main');
+		var leftPanel = $('.panel-left');
+		var scrollBtn = $('#scroll-btn');
+		var menuBtn = $('.icon-strips');
+
+		body.classList.remove('loading');
+
+		$.on(scrollBtn, 'tap', riseInfo);
+		$.on(menuBtn, 'tap', autoPanel);
+
+		//防止在菜单上滑动导致右边界面滚动
+		$.on(leftPanel, 'touchmove', function (e) {
+			e.preventDefault();
+		});
+
+		$.on(leftPanel, 'click', function (e) {
+			var target = e.target;
+
+			if (target.tagName === 'A') {
+				var elem = $(target.dataset.href);
+				var top = elem.offsetTop;
+				var curPos = $('body').scrollTop;
+
+				riseInfo();
+				autoPanel(e, true);
+				setTimeout(function () {
+					var timer = setInterval(function () {
+						if (curPos < top) {
+							curPos += 10;
+							$('body').scrollTop = curPos;
+						} else {
+							$('body').scrollTop = top;
+							clearInterval(timer);
+						}
+					}, 1);
+				}, 500);
+			}
+		});
+
+		function riseInfo() {
+			mainPanel.classList.add('panel-main-free');
+		}
+
+		function autoPanel(e, notPreventDefault) {
+			var leftPanelWidth = leftPanel.offsetWidth;
+
+			e.stopPropagation();
+			!notPreventDefault && e.preventDefault();
+
+			if (body.classList.contains('panel-on')) {
+				body.classList.remove('panel-on');
+			} else {
+				body.classList.add('panel-on');
+				//点击右侧界面隐藏菜单
+				$.on(mainPanel, 'tap', function handler(e) {
+					e.preventDefault();
+					body.classList.remove('panel-on');
+					$.off(mainPanel, 'tap', handler);
+				});
+			}
 		}
 	}
 
 	checkReadyState();
 	$.on(document, 'readystatechange', checkReadyState);
-
-	$.on(scrollBtn, 'click', function () {
-		mainPanel.classList.add('panel-main-free');
-	});
 });
